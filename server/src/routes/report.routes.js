4a4142997d6e14ea7dcc56d0f30446ee06770025
saarth-24 <saarthsingh24@gmail.com>
2026-05-09 12:08:35 +0530

    Added n8n automation

--- server/src/routes/report.routes.js
+++ server/src/routes/report.routes.js
@@ -1,5 +1,4 @@
 import express from "express";
-<<<<<<< HEAD
 import { createReport, 
     updateReportStatus, 
     getReports} from "../controller/report.controller.js";
@@ -9,19 +8,6 @@ const router = express.Router();
 
 router.post("/", upload.single("image"), createReport);
 router.get("/",getReports);
-=======
-import { createReport,
-    getReports,
-    updateReportStatus 
-} from "../controller/report.controller.js";
-import upload from "../middelware/upload.middleware.js";
-// import { updateReportStatus } from "../controller/report.controller.js";
-    
-const router = express.Router();
-
-router.post("/", upload.single("image"), createReport);
-router.get("/", getReports);
->>>>>>> b25d31a06369e67be17e7408f6bcdf1ecd288c86
 router.patch("/:id/status",updateReportStatus);
 
 export default router;
